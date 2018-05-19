7f5ab22ff42dfe129a3113d59d459709bd057897
Miguel Rueda <mrueda@thoughtworks.com>
2018-05-19 14:12:26 -0500

    creating initial model for the posts

--- admin_blog/blog/models.py
+++ admin_blog/blog/models.py
@@ -1,3 +1,22 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+
+    DEVELOPER = 'developer'
+    DEVOPS = 'devops'
+    AUTOMATION = 'automation'
+
+    CATEGORY_CHOICES = (
+        (DEVELOPER, 'Developer'),
+        (DEVOPS, 'DevOps'),
+        (AUTOMATION, 'Developer')
+    )
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=40)
+    content = models.CharField(max_length=500)
+    creation_date = models.DateField()
+    publish_date = models.DateField()
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
