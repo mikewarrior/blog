e48a1e909b620a488790634ec089bd4b00f40aa2
Miguel Rueda <mrueda@thoughtworks.com>
2018-05-20 10:05:17 -0500

    adding search filter per author, content and title

--- admin_blog/blog/api/views.py
+++ admin_blog/blog/api/views.py
@@ -4,17 +4,27 @@ from rest_framework.generics import(
     RetrieveAPIView,
     DestroyAPIView,
     UpdateAPIView
-    )
+    )x
 from blog.api.models import Post
 from .serializers import PostSerializer
+from django.db.models import Q
 
 class PostCreateView(CreateAPIView):
     ueryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostListView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get('q')
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query)|
+                Q(content__icontains=query)|
+                Q(author__icontains=query)).distinct()
+        return queryset_list
 
 class PostDetailView(RetrieveAPIView):
     queryset = Post.objects.all()
